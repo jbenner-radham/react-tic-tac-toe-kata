d60690471bc166ecdfb79eab449f48bef9a54824
James Benner <james.benner@gmail.com>
2019-02-17 11:39:18 -0500

    Added cell renders value test.

--- src/components/Cell.test.js
+++ src/components/Cell.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Cell from './Cell';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Cell/>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the value passed to it', () => {
+    const value = 'X';
+
+    expect(shallow(<Cell value={value}/>).text()).toBe(value);
 });
