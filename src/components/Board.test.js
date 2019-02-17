91a342b17bd4a84aba30ba0df41697a64dd423a5
James Benner <james.benner@gmail.com>
2019-02-17 10:26:35 -0500

    Added board has 9 cells test.

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import Board from './Board';
+
+const wrapper = mount(<Board/>);
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Board/>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('contains nine <Cell/> components', () => {
+    expect(wrapper.find('.Cell')).toHaveLength(9);
 });
