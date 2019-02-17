70e3033bf5b5ce07888f57b710eaa48cd91170a0
James Benner <james.benner@gmail.com>
2019-02-17 13:46:39 -0500

    Added app renders game component test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
+
     ReactDOM.render(<App/>, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a <Game/> component', () => {
+    expect(mount(<App/>).find('.Game').length).toBe(1);
 });
