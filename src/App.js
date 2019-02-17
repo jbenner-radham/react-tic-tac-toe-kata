d27b1d3a28a9779af9b92d55646d0c1a99c44048
James Benner <james.benner@gmail.com>
2019-02-17 13:48:14 -0500

    Implemented app renders game component spec.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Board from './components/Board';
+import Game from './components/Game';
 import './App.scss';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Board/>
+                <Game/>
                 <h1 className="App__heading">Tic-Tac-Toe!</h1>
             </div>
         );
