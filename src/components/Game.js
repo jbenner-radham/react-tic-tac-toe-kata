86eeb91750645e85ec883a5ac4618e7b4ca3c632
James Benner <james.benner@gmail.com>
2019-02-17 16:41:43 -0500

    Implemented game getWinner returns false spec.

--- src/components/Game.js
+++ src/components/Game.js
@@ -28,7 +28,38 @@ export default class Game extends React.Component {
     }
 
     getWinner(cells) {
+        const winAxes = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
 
+        const xWon = winAxes.reduce((accumulator, axis) => (
+            accumulator
+                ? accumulator
+                : axis.every(index => cells[index] === 'X')
+        ), false);
+
+        if (xWon) {
+            return 'X';
+        }
+
+        const oWon = winAxes.reduce((accumulator, axis) => (
+            accumulator
+                ? accumulator
+                : axis.every(index => cells[index] === 'O')
+        ), false);
+
+        if (oWon) {
+            return 'O';
+        }
+
+        return false;
     }
 
     render() {
