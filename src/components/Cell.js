c999cc5c5aa707c77c4a5251a6c9ec8ff346021e
James Benner <james.benner@gmail.com>
2019-02-17 11:41:14 -0500

    Implemented cell renders value spec.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Cell.scss';
 
 export default class Cell extends React.Component {
-    render() {
+    render(props) {
         return (
-            <div className="Cell"></div>
+            <div className="Cell">
+                {this.props.value}
+            </div>
         );
     }
 }
