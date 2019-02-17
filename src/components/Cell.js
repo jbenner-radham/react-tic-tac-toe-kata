38b34479ee3ade216c88fc415e3a1b61c2572f45
James Benner <james.benner@gmail.com>
2019-02-17 10:11:56 -0500

    Implemented cell render spec.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import './Cell.scss';
 
-export default class Cell extends React.Component {}
+export default class Cell extends React.Component {
+    render() {
+        return (
+            <div className="Cell"></div>
+        );
+    }
+}
